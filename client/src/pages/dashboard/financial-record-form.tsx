43d985b6f073b08c0125af4f575f2a96c125c20d
RomchukAnton <romchuk.work@outlook.com>
2024-06-28 12:33:26 +0200

    financial-record-form changes in frontend

--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -1,22 +1,56 @@
-
-
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
 
 
 
 export const FinancialRecordForm = () => {
+
+
+    const [description, setDescription] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [paymentMethod, setPaymentMethod] = useState<string>("");
+
+    const { user } = useUser();
+
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const newRecord = {
+            userId: user?.id,
+            date: new Date(),
+            description: description,
+            amount: parseFloat(amount),
+            category: category,
+            paymentMethod: paymentMethod,
+        };
+
+        //addRecord(newRecord);
+        setDescription("");
+        setAmount("");
+        setCategory("");
+        setPaymentMethod("");
+
+    };
+
+
+
+
+
     return <div className="form-container">
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-field">
                 <label>Description:</label>
-                <input type="text" required className="input" />
+                <input type="text" required className="input" value={description} onChange={(event) => setDescription(event.target.value)} />
             </div>
             <div className="form-field">
                 <label>Amount:</label>
-                <input type="number" required className="input" />
+                <input type="number" required className="input" value={amount} onChange={(event) => setAmount(event.target.value)} />
             </div>
             <div className="form-field">
                 <label>Category:</label>
-                <select required className="input">
+                <select required className="input" value={category} onChange={(event) => setCategory(event.target.value)}>
                     <option value="">Select a Category</option>
                     <option value="Food">Food</option>
                     <option value="Rent">Rent</option>
@@ -28,7 +62,7 @@ export const FinancialRecordForm = () => {
             </div>
             <div className="form-field">
                 <label>Payment Method:</label>
-                <select required className="input">
+                <select required className="input" value={paymentMethod} onChange={(event) => setPaymentMethod(event.target.value)}>
                     <option value="">Select a Payment Method</option>
                     <option value="Credit Card">Credit Card</option>
                     <option value="Cash">Cash</option>
