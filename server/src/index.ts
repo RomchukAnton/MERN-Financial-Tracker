c8a08fe150045015c2dab0723d6784b7f7003aea
RomchukAnton <romchuk.work@outlook.com>
2024-06-28 14:55:14 +0200

    connection to mongo db

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,5 +6,16 @@ import dotenv from "dotenv";
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3001;
+const mongoURI: any = process.env.MONGODB_CONNECT;
 
 app.use(express.json());
+
+
+
+
+mongoose.connect(mongoURI)
+    .then(() => console.log("CONNECTED TO MONGODB !"))
+    .catch((err) => console.error("Failed to Connect to MongoDb", err));
+
+
+app.listen(port, () => console.log(`Server Running on Port ${port}`));
